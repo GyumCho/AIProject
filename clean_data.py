35b1c1366cf77bfa68daf1d14a90d694f83a9d1f
socraates <t.bostelaar@student.utwente.nl>
2023-10-06 11:21:53 +0200

    filter dataset by languages complete

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 csv_file = pd.read_csv("new_data.csv", encoding='unicode_escape')
 dataset = csv_file[['id','native','language','probabilities','english_only','text']]
@@ -8,12 +9,24 @@ print(f'Orginal Length: {len(dataset)}')
 dup_removed = dataset.drop_duplicates(['native','language','text'])
 print(f'New Length: \t{len(dup_removed)}')
 
+#dup_removed.to_csv('new_data_clean.csv')
+#ds = pd.read_csv("new_data_clean.csv", encoding='unicode_escape')
 
-# selecting languages
-mask = ((dup_removed['native'] == 'French') | (dup_removed['native'] == 'Spanish'))
-dataset_selection = dup_removed[mask]
+ds = dup_removed.copy()
+
+# grouping dialects 
+ds['native'].mask(ds['native'] == 'Basque<br/>Spanish', 'Spanish<br/>Basque', inplace=True)
+ds['native'].mask(ds['native'] == 'Catalan<br/>Spanish', 'Spanish<br/>Catalan', inplace=True)
+ds['native'].mask(ds['native'] == 'Galician<br/>Spanish', 'Spanish<br/>Galician', inplace=True)
+ds['native'].mask(ds['native'] == 'Italian<br/>Spanish', 'Spanish<br/>Italian', inplace=True)
+
+
+# filtering by language
+la_list = ['Spanish<br/>Catalan','Spanish<br/>Galician',
+'Spanish<br/>Basque','Spanish<br/>Italian', 'Italian', 'French','Spanish']
+mask = ds['native'].isin(la_list)
+dataset_selection = ds[mask]
 print(len(dataset_selection))
+print(dataset_selection['native'].value_counts())
 
-# count values
-#print(dup_removed['native'].value_counts().to_string())
-# dup_removed.to_csv('new_data_clean')
+dataset_selection.to_csv('data_selected_clean.csv')
